48192fa227355e4c7333b44aa4227fde248c99d3
Srikar Kolluru <101688776+kollurusrikar@users.noreply.github.com>
2023-03-27 06:23:13 +0000

    s

---
+++ MODULE-3/array.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class arrays
+{
+    public static void main(String args[])
+    {
+        int n,i;
+        Scanner input=new Scanner(System.in);
+        System.out.println("enter the no of elements in array: ");
+        n=input.nextInt();
+        int marks[]=new int[n];
+        for(i=0;i<n;i++)
+        {
+            marks[i]=input.nextInt;
+            System.out.println(marks[i]);
+
+        }
+    }
+}
